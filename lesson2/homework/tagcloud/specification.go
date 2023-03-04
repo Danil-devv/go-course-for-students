8451d339861f2c0b59f01454d9a2432eb5e26d63
danil-devv <s3menovd4nil@yandex.ru>
2023-03-04 20:19:01 +0300

    done homework2

--- lesson2/homework/tagcloud/specification.go
+++ lesson2/homework/tagcloud/specification.go
@@ -2,36 +2,94 @@ package tagcloud
 
 // TagCloud aggregates statistics about used tags
 type TagCloud struct {
-	// TODO: add fields if necessary
+	// Словарь состоит из пар вида тег-индекс
+	// Тег является строкой
+	// map[tag] - это индекс вхождения тега tag в слайс occurrence
+	tagIndexInOccurrenceArray map[string]int
+
+	// Слайс частоты появления тегов, отсортированный по убыванию
+	// occurrence[i] - это частота появления тега с индексом i
+	// ПРИМЕР: occurrence[tagIndexInOccurrenceArray["t1"]] - частота появления тега "t1"
+	occurrence []int
+
+	// Слайс тегов
+	// indexToTag[i] возвращает тег(строку), частотность которого равна occurrence[i]
+	// ПРИМЕР: indexToTag[5] - тег, который встречается occurrence[5] раз
+	indexToTag []string
+
+	// Данный метод хранения тегов необходим для того, чтобы не хранить
+	// одинаковые теги по несколько экземпляров и быстро добавлять тег в облако,
+	// а также быстро находить N самых частотных тегов
+
+	// O(log N) - асимптотика функции AddTag(tag)
+	// O(n) - асимптотика функции TopN(n)
 }
 
-// TagStat represents statistics regarding single tag
+// TagStat represent statistics regarding single tag
 type TagStat struct {
 	Tag             string
 	OccurrenceCount int
 }
 
-// New should create a valid TagCloud instance
-// TODO: You decide whether this function should return a pointer or a value
+// New create a valid TagCloud instance
 func New() TagCloud {
-	// TODO: Implement this
-	return TagCloud{}
+	return TagCloud{
+		tagIndexInOccurrenceArray: make(map[string]int),
+		occurrence:                make([]int, 0),
+		indexToTag:                make([]string, 0),
+	}
 }
 
-// AddTag should add a tag to the cloud if it wasn't present and increase tag occurrence count
-// thread-safety is not needed
-// TODO: You decide whether receiver should be a pointer or a value
-func (TagCloud) AddTag(tag string) {
-	// TODO: Implement this
+// AddTag add a tag to the cloud if it wasn't present and increase tag occurrence count
+func (cloud *TagCloud) AddTag(tag string) {
+	index, ok := cloud.tagIndexInOccurrenceArray[tag]
+	if ok {
+		cloud.occurrence[index]++
+
+		// После увеличения частотности тега возможна такая ситуация,
+		// что слайс occurrence больше не отсортирован по убыванию.
+		// В таком случае есть ровно один элемент, с которым нам нужно
+		// поменять местами occurrence[index], чтобы слайс снова стал остортированным.
+		// Воспользуемся для этого бинарным поиском
+		left, right := 0, index
+		for left < right {
+			mid := (left + right) / 2
+			if cloud.occurrence[mid] >= cloud.occurrence[index] {
+				left = mid + 1
+			} else {
+				right = mid
+			}
+		}
+
+		// Смена местами тега с индексом index и тега с индексом left
+		cloud.occurrence[left], cloud.occurrence[index] = cloud.occurrence[index], cloud.occurrence[left]
+		cloud.indexToTag[left], cloud.indexToTag[index] = cloud.indexToTag[index], cloud.indexToTag[left]
+		cloud.tagIndexInOccurrenceArray[tag] = left
+		cloud.tagIndexInOccurrenceArray[cloud.indexToTag[index]] = index
+
+	} else {
+		// Если тег встречается впервые, то его необходимо просто добавить в конец массива occurrence
+		cloud.tagIndexInOccurrenceArray[tag] = len(cloud.occurrence)
+		cloud.occurrence = append(cloud.occurrence, 1)
+		cloud.indexToTag = append(cloud.indexToTag, tag)
+	}
 }
 
-// TopN should return top N most frequent tags ordered in descending order by occurrence count
-// if there are multiple tags with the same occurrence count then the order is defined by implementation
-// if n is greater that TagCloud size then all elements should be returned
-// thread-safety is not needed
-// there are no restrictions on time complexity
-// TODO: You decide whether receiver should be a pointer or a value
-func (TagCloud) TopN(n int) []TagStat {
-	// TODO: Implement this
-	return nil
+// TopN return top N most frequent tags ordered in descending order by occurrence count
+func (cloud *TagCloud) TopN(n int) []TagStat {
+	// Обработка случая, когда N больше кол-ва тегов в облаке
+	if n > len(cloud.occurrence) {
+		n = len(cloud.occurrence)
+	}
+
+	top := make([]TagStat, n)
+
+	// Так как массив occurrence отсортирован по убыванию,
+	// нам достаточно взять первые N тегов
+	for i := 0; i < n; i++ {
+		top[i].Tag = cloud.indexToTag[i]
+		top[i].OccurrenceCount = cloud.occurrence[i]
+	}
+
+	return top
 }
