918a08d3f89d08d1531c8fcf93f804e627c05abf
danil-devv <s3menovd4nil@yandex.ru>
2023-03-14 02:30:38 +0300

    realised version with block-size support
    WARNING: this version doesn't support trim_spaces flag

--- lesson3/homework/main.go
+++ lesson3/homework/main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
-	"bufio"
 	"errors"
 	"flag"
 	"fmt"
 	"io"
+	"math"
 	"os"
 	"strings"
 )
+
+type ReadWriteSeekCloser interface {
+	io.ReadWriter
+	io.Seeker
+	io.Closer
+}
 
 // Options хранит настройки чтения и записи
 type Options struct {
@@ -26,8 +32,8 @@ func (opts *Options) SetDefault() {
 	opts.To = ""
 	opts.Conv = make(map[string]bool)
 	opts.Offset = 0
-	opts.Limit = -1
-	opts.BlockSize = -1
+	opts.Limit = math.MaxInt64
+	opts.BlockSize = 4096
 }
 
 // validateFlags проверяет все флаги на валидность
@@ -75,11 +81,11 @@ func validateFlags(options *Options) error {
 		return errors.New("the value of offset must be positive")
 	}
 
-	if options.Limit != -1 && options.Limit < 0 {
+	if options.Limit < 0 {
 		return errors.New("the count of bytes to read must be positive")
 	}
 
-	if options.BlockSize != -1 && options.BlockSize <= 0 {
+	if options.BlockSize <= 0 {
 		return errors.New("the block size must be positive")
 	}
 
@@ -94,8 +100,8 @@ func ParseFlags() (*Options, error) {
 	flag.StringVar(&opts.From, "from", "", "file to read. by default - stdin")
 	flag.StringVar(&opts.To, "to", "", "file to write. by default - stdout")
 	flag.Int64Var(&opts.Offset, "offset", 0, "count of bytes to skip. by default - 0")
-	flag.Int64Var(&opts.Limit, "limit", -1, "max count of bytes to read. by default - -1")
-	flag.Int64Var(&opts.BlockSize, "block-size", -1, "max count of bytes to read and write."+
+	flag.Int64Var(&opts.Limit, "limit", math.MaxInt64, "max count of bytes to read. by default - -1")
+	flag.Int64Var(&opts.BlockSize, "block-size", 4, "max count of bytes to read and write."+
 		" by default - -1")
 	conv := flag.String("conv", "", "conversions over text: upper_case, lower_case and trim_spaces")
 
@@ -112,12 +118,12 @@ func ParseFlags() (*Options, error) {
 	return &opts, err
 }
 
-// OpenFile возвращает поток ввода/вывода типа io.ReadWriteCloser
+// OpenFile возвращает поток ввода/вывода типа ReadWriteSeekCloser
 // filepath - путь к файлу
 // если readMode == true, возвращается поток ввода, иначе - поток вывода
-func OpenFile(filePath string, readMode bool) (io.ReadWriteCloser, error) {
+func OpenFile(filePath string, readMode bool) (ReadWriteSeekCloser, error) {
 	var (
-		stream io.ReadWriteCloser
+		stream ReadWriteSeekCloser
 		err    error
 	)
 
@@ -138,86 +144,67 @@ func OpenFile(filePath string, readMode bool) (io.ReadWriteCloser, error) {
 	return stream, err
 }
 
-// ReadData считывает limit байт из потока ввода stream,
-// пропуская при этом offset байт из начала ввода
-func ReadData(stream io.ReadWriteCloser, offset int64, limit int64) ([]byte, error) {
+func ConvertData(data *[]byte, conv *map[string]bool) {
+	buffer := string(*data)
+
+	if (*conv)["upper_case"] {
+		buffer = strings.ToUpper(buffer)
+	}
+
+	if (*conv)["lower_case"] {
+		buffer = strings.ToLower(buffer)
+	}
+
+	*data = []byte(buffer)
+}
+
+func PipeData(inputStream ReadWriteSeekCloser, outputStream ReadWriteSeekCloser,
+	offset int64, limit int64, conv map[string]bool, blockSize int64) error {
 	var (
 		err  error
 		data []byte
 	)
 
-	reader := bufio.NewReader(stream)
-	// пропуск offset байт из начала ввода
-	_, err = reader.Discard(int(offset))
-	if err != nil {
-		return data, errors.New("offset is bigger than file size." +
-			" unable to read the file")
-	}
-
-	// по умолчанию limit равен -1, поэтому стоит такое условие
-	// точно известно, что если limit > 0 изначально, то в какой-то момент либо закончится файл,
-	// либо limit станет равен нулю и ввод завершится
-	for limit != 0 {
-		readedByte, err := reader.ReadByte()
-
-		if err == io.EOF {
-			break
-		}
-
+	// выполняем смещение потока ввода на offset байт
+	data = make([]byte, 1)
+	for offset > 0 {
+		_, err := inputStream.Read(data)
+		offset--
 		if err != nil {
-			return data, errors.New("unable to read byte")
+			return errors.New("offset is bigger than file size." +
+				" unable to read the file")
 		}
-		data = append(data, readedByte)
-		limit--
 	}
 
-	// закрытие потока ввода
-	defer func(stream io.ReadWriteCloser) {
-		err := stream.Close()
-		if err != nil {
-			_, _ = fmt.Fprintln(os.Stderr, "can not close a file:", err)
-			os.Exit(1)
+	if !conv["trim_space"] {
+		for limit > 0 {
+			data = make([]byte, blockSize)
+
+			size, err := inputStream.Read(data)
+
+			if err == io.EOF {
+				break
+			}
+
+			if err != nil {
+				return err
+			}
+
+			if int64(size) < blockSize {
+				data = data[:size]
+			}
+
+			ConvertData(&data, &conv)
+
+			_, err = outputStream.Write(data)
+
+			if err != nil {
+				return err
+			}
+
+			limit -= int64(size)
 		}
-	}(stream)
-
-	return data, err
-}
-
-// ConvertData форматирует массив байт согласно ключам в словаре conv
-func ConvertData(data []byte, conv map[string]bool) []byte {
-	buf := string(data)
-	if conv["trim_spaces"] {
-		buf = strings.TrimSpace(buf)
 	}
-
-	if conv["upper_case"] {
-		buf = strings.ToUpper(buf)
-	}
-
-	if conv["lower_case"] {
-		buf = strings.ToLower(buf)
-	}
-
-	return []byte(buf)
-}
-
-// WriteData записывает все байты из массива data в поток вывода stream
-func WriteData(stream io.ReadWriteCloser, data []byte) error {
-	var err error
-	writer := bufio.NewWriter(stream)
-	_, err = writer.Write(data)
-	if err != nil {
-		return err
-	}
-
-	// закрытие и сохранение потока вывода
-	defer func(w *bufio.Writer) {
-		err = w.Flush()
-		if err != nil {
-			_, _ = fmt.Fprintln(os.Stderr, "Error: ", err)
-			os.Exit(1)
-		}
-	}(writer)
 
 	return err
 }
@@ -226,7 +213,7 @@ func main() {
 	// парсим флаги
 	opts, err := ParseFlags()
 	if err != nil {
-		_, _ = fmt.Fprintln(os.Stderr, "Error: ", err)
+		_, _ = fmt.Fprintln(os.Stderr, "flags are not valid: ", err)
 		os.Exit(1)
 	}
 
@@ -237,18 +224,6 @@ func main() {
 		os.Exit(1)
 	}
 
-	// data - массив данных из потока ввода
-	var data []byte
-	// считываем данные из потока ввода
-	data, err = ReadData(inputStream, opts.Offset, opts.Limit)
-	if err != nil {
-		_, _ = fmt.Fprintln(os.Stderr, "Error: ", err)
-		os.Exit(1)
-	}
-
-	// форматируем данные
-	data = ConvertData(data, opts.Conv)
-
 	// создаем поток вывода outputStream
 	outputStream, err := OpenFile(opts.To, false)
 	if err != nil {
@@ -256,10 +231,26 @@ func main() {
 		os.Exit(1)
 	}
 
-	// записываем данные в поток вывода
-	err = WriteData(outputStream, data)
+	// выполняем чтение и запись информации
+	err = PipeData(inputStream, outputStream, opts.Offset,
+		opts.Limit, opts.Conv, opts.BlockSize)
 	if err != nil {
-		_, _ = fmt.Fprintln(os.Stderr, "Error: ", err)
+		_, _ = fmt.Fprintln(os.Stderr, "an error occurred while reading/writing data: ", err)
 		os.Exit(1)
 	}
+
+	// закрытие и сохранение потока вывода и вывода
+	defer func(in *ReadWriteSeekCloser, out *ReadWriteSeekCloser) {
+		err := (*in).Close()
+		if err != nil {
+			_, _ = fmt.Fprintln(os.Stderr, "can not close an input stream:", err)
+			os.Exit(1)
+		}
+
+		err = (*out).Close()
+		if err != nil {
+			_, _ = fmt.Fprintln(os.Stderr, "can not close an output stream:", err)
+			os.Exit(1)
+		}
+	}(&inputStream, &outputStream)
 }
