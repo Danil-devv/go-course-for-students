9638c0614995bb6ccbd64dd795f2aa4b07e87e94
danil-devv <s3menovd4nil@yandex.ru>
2023-03-21 12:11:44 +0300

    hw4 fixes
    added -race flag to tests
    added mutex for error reading/writing
    changed maxWorkerCount default value to 4

--- lesson4/homework/storage/dir_sizer.go
+++ lesson4/homework/storage/dir_sizer.go
@@ -24,21 +24,21 @@ type DirSizer interface {
 // sizer implement the DirSizer interface
 type sizer struct {
 	// maxWorkersCount number of workers for asynchronous run
-	// by default - 100
+	// by default - 4
 	maxWorkersCount int
 }
 
 // NewSizer returns new DirSizer instance
 func NewSizer() DirSizer {
 	return &sizer{
-		maxWorkersCount: 100,
+		maxWorkersCount: 4,
 	}
 }
 
 // worker обрабатывает указатели на массивы файлов из канала filesChan.
 // увеличивает кол-во обработанных файлов count и общий вес файлов count,
 // используя атомарные инструкции
-func worker(ctx context.Context, filesChan chan *[]File, wg *sync.WaitGroup,
+func worker(ctx context.Context, filesChan chan *[]File, wg *sync.WaitGroup, m *sync.Mutex,
 	size *int64, count *int64, runtimeError *error) {
 	defer wg.Done()
 
@@ -59,7 +59,9 @@ func worker(ctx context.Context, filesChan chan *[]File, wg *sync.WaitGroup,
 				// если произошла ошибка при работе с файлом,
 				// то записываем ошибку в runtimeError и останавливаем горутину
 				if err != nil {
+					m.Lock()
 					*runtimeError = fmt.Errorf("file does not exist: %v", err)
+					m.Unlock()
 					return
 				}
 				// атомарно увеличиваем size и count
@@ -73,8 +75,8 @@ func worker(ctx context.Context, filesChan chan *[]File, wg *sync.WaitGroup,
 // dirsAdder это горутина, которая добавляет файлы из директории d в канал файлов.
 // При этом она может вызывать себя рекурсивно,
 // если dirAddersCount не превосходит maxWorkersCount
-func dirsAdder(ctx context.Context, filesChan chan *[]File, wg *sync.WaitGroup, dir Dir,
-	dirAddersCount *int64, maxWorkersCount int, runtimeError *error) {
+func dirsAdder(ctx context.Context, filesChan chan *[]File, wg *sync.WaitGroup, m *sync.Mutex,
+	dir Dir, dirAddersCount *int64, maxWorkersCount int, runtimeError *error) {
 	// после завершения необходимо уменьшить счетчик работающих adder'ов на 1
 	defer func() {
 		atomic.AddInt64(dirAddersCount, -1)
@@ -92,7 +94,9 @@ func dirsAdder(ctx context.Context, filesChan chan *[]File, wg *sync.WaitGroup, 
 		// который читает этот канал
 		filesChan <- &files
 		if err != nil {
+			m.Lock()
 			*runtimeError = fmt.Errorf("file does not exist: %v", err)
+			m.Unlock()
 			return
 		}
 
@@ -106,7 +110,7 @@ func dirsAdder(ctx context.Context, filesChan chan *[]File, wg *sync.WaitGroup, 
 					// Если у нас есть пространство для создания еще одной горутины, то мы создаём ее
 					atomic.AddInt64(dirAddersCount, 1)
 					wg.Add(1)
-					go dirsAdder(ctx, filesChan, wg, d, dirAddersCount, maxWorkersCount, runtimeError)
+					go dirsAdder(ctx, filesChan, wg, m, d, dirAddersCount, maxWorkersCount, runtimeError)
 				} else {
 					// Если мы не можем создать еще одну горутину, то тогда
 					// функция сама обработает все папки из директории
@@ -127,6 +131,7 @@ func (a *sizer) Size(ctx context.Context, d Dir) (Result, error) {
 		filesChan    chan *[]File   // поток с файлами
 		addersCount  int64          // кол-во горутин-adder'ов
 		runtimeError error          // сюда будем записывать ошибку, которая могла произойти в рантайме
+		m            sync.Mutex     // мьютекс для блокировки записи в runtimeError
 	)
 
 	// контекст с отменой используется в горутинах worker и adder
@@ -138,14 +143,14 @@ func (a *sizer) Size(ctx context.Context, d Dir) (Result, error) {
 	// половину горутин выделяем для worker'ов
 	for i := 1; i <= a.maxWorkersCount/2; i++ {
 		workersWG.Add(1)
-		go worker(ctxWithCancel, filesChan, &workersWG, &size, &count, &runtimeError)
+		go worker(ctxWithCancel, filesChan, &workersWG, &m, &size, &count, &runtimeError)
 	}
 
 	// запускаем adder, который будет добавлять в канал файлов новые файлы
 	// так же dirsAdder может рекурсивно вызывать себя в качестве горутины,
 	// поэтому мы оставили свободное место размером maxWorkersCount/2
 	addersWG.Add(1)
-	go dirsAdder(ctxWithCancel, filesChan, &addersWG, d, &addersCount, a.maxWorkersCount/2, &runtimeError)
+	go dirsAdder(ctxWithCancel, filesChan, &addersWG, &m, d, &addersCount, a.maxWorkersCount/2, &runtimeError)
 
 	addersWG.Wait()
 	// когда все adder'ы закончили работу и добавили все файлы из директории в канал,
@@ -154,8 +159,13 @@ func (a *sizer) Size(ctx context.Context, d Dir) (Result, error) {
 
 	// если в рантайме произошла какая-то ошибка,
 	// то завершаем контекст со всеми работающими горутинами
-	if runtimeError != nil {
+	m.Lock()
+	switch runtimeError {
+	case nil:
+		m.Unlock()
+	default:
 		cancel()
+		m.Unlock()
 	}
 
 	// ожидаем, пока все worker'ы закончат свою работу
